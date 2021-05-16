93e4a6aaf90a374d5d7fb6119f5974097d6fc937
Alexander Holub <holub.alexander.it@gmail.com>
2021-05-16 23:36:14 +0300

    feat: implement user router

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -2,10 +2,59 @@ const router = require('express').Router();
 const User = require('./user.model');
 const usersService = require('./user.service');
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
+router.route('/').get(async (req, res, next) => {
+  try {
+    const users = await usersService.getAll();
+
+    res.status(200);
+    res.json(users.map(User.toResponse));
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.route('/:userId').get(async (req, res, next) => {
+  try {
+    const user = await usersService.getUser(req.params.userId);
+
+    res.status(200);
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.route('/').post(async (req, res, next) => {
+  try {
+    const user = await usersService.addUser(req.body);
+
+    res.status(201);
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.route('/:userId').put(async (req, res, next) => {
+  try {
+    const updatedUser = await usersService.updateUser(req.params.userId, req.body);
+
+    res.status(200);
+    res.json(updatedUser);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.route('/:userId').delete(async (req, res, next) => {
+  try {
+    await usersService.deleteUser(req.params.userId);
+
+    res.status(200);
+    res.send({ message: 'Success delete' });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,34 @@
 const usersRepo = require('./user.memory.repository');
+const User = require('./user.model');
 
 const getAll = () => usersRepo.getAll();
 
-module.exports = { getAll };
+const getUser = async (userId) => {
+  let res = await usersRepo.getUser(userId) ?? null;
+
+  if (res) {
+    res = User.toResponse(res);
+  }
+
+  return res;
+}
+
+const addUser = async (user) => User.toResponse(await usersRepo.addUser(new User(user)));
+
+const updateUser = async (userId, data = {}) => {
+  let updatedUser = await usersRepo.updateUser(userId, data);
+
+  if (updatedUser) {
+    updatedUser = User.toResponse(updatedUser);
+  }
+
+  return updatedUser;
+};
+
+const deleteUser = async (userId) => usersRepo.deleteUser(userId);
+
+module.exports = { 
+  getAll, getUser, 
+  addUser, updateUser,
+  deleteUser,
+};
